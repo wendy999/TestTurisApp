8a9d347752729586e85e91adcd21a09bb2562725
android <wendyvanessa.am@gmail.com>
2018-09-08 16:07:57 -0500

    creacion menu2

--- app/src/main/java/com/example/android/testturisapp/menu/splash/menu/MenuActivity.java
+++ app/src/main/java/com/example/android/testturisapp/menu/splash/menu/MenuActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.android.testturisapp.R;
+
 public class MenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
