21304497083e99ac0f046f5cfe9227c01d819081
android <anyta2106@hotmail.com>
2018-09-08 16:46:02 -0500

    Logo-Splash-icon

--- app/src/main/java/com/example/android/testturisapp/menu/splash/SplashActivity.java
+++ app/src/main/java/com/example/android/testturisapp/menu/splash/SplashActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.testturisapp.menu.splash;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.android.testturisapp.R;
+import com.example.android.testturisapp.menu.splash.menu.MenuActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -13,6 +16,15 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this, MenuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
 
     }
 }
