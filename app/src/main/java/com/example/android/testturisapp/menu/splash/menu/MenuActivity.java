de0c20312192958893e03b44cf7fb1bf9f8cea61
android <anyta2106@hotmail.com>
2018-09-08 17:40:39 -0500

    master_detail_restaurants

--- app/src/main/java/com/example/android/testturisapp/menu/splash/menu/MenuActivity.java
+++ app/src/main/java/com/example/android/testturisapp/menu/splash/menu/MenuActivity.java
@@ -82,20 +82,15 @@ public class MenuActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+         if (id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_sites) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_hotel) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_restaurant) {
 
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
