3789769a8ec5992e35e185a5cfe159e2bb4143d0
android <anyta2106@hotmail.com>
2018-09-09 23:05:57 -0500

    Layout_about_us

---
+++ app/src/main/java/com/example/android/testturisapp/aboutus/AboutUsActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.testturisapp.aboutus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.testturisapp.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
